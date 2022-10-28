f09105ee84c5f84d49e4630fdf5ce3b7e2f42bda
3lang <675483520@qq.com>
2022-10-28 13:15:53 +0800

    chore: test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
